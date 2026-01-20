5c3a010e00c97a9051f3321d0a56c37172ce93ef
hassaanahmedqureshi <hassaanahmedqureshi@gmail.com>
2026-01-20 09:50:06 +0100

    feat: implement dynamic feature flag filtering and support loading all flags on initialization

--- frontend/src/app/components/dashboard/dashboard.ts
+++ frontend/src/app/components/dashboard/dashboard.ts
@@ -10,6 +10,7 @@ import { FeatureFlag, FeatureFlagService } from '../../services/feature-flag';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  allFlags: FeatureFlag[] = [];
   flags: FeatureFlag[] = [];
   environment: string = 'all';
 
@@ -18,22 +19,31 @@ export class Dashboard {
   }
 
   ngOnInit(): void {
-    this.loadFlags();
+    this.loadAllFlags();
   }
 
-  loadFlags(): void {
-    this.flagService.getFlags(this.environment).subscribe(data => {
-      this.flags = data;
+  loadAllFlags(): void {
+    this.flagService.getFlags('all').subscribe(data => {
+      this.allFlags = data;
+      this.filterFlags();
     });
+  }
+
+  filterFlags(): void {
+    if (this.environment === 'all') {
+      this.flags = this.allFlags;
+    } else {
+      this.flags = this.allFlags.filter(flag => flag.environment === this.environment);
+    }
   }
 
   toggle(flag: FeatureFlag): void {
     this.flagService.toggleFlag(flag.id).subscribe(() => {
-      flag.enabled = !flag.enabled; // update locally
+      flag.enabled = !flag.enabled;
     });
   }
 
   onEnvChange(): void {
-    this.loadFlags();
+    this.filterFlags();
   }
 }
